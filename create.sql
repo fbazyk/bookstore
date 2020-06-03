f9f7660439a45d02b4432bb6b8efaef1e82f86ab
FBYBK60 <fedor.bazyk@realdolmen.com>
2020-06-03 16:00:54 +0200

    change in Games coulmn name min_age => minage

--- create.sql
+++ create.sql
@@ -1,5 +1,5 @@
 create table books (id  bigserial not null, price numeric(19, 2) not null, supplier_id varchar(100) not null, title varchar(100) not null, author varchar(100), isbn varchar(255) not null, pages int8, primary key (id));
-create table games (id  bigserial not null, price numeric(19, 2) not null, supplier_id varchar(100) not null, title varchar(100) not null, genre varchar(255), min_age int8, publisher varchar(100), primary key (id));
+create table games (id  bigserial not null, price numeric(19, 2) not null, supplier_id varchar(100) not null, title varchar(100) not null, genre varchar(255), minage int8, publisher varchar(100), primary key (id));
 create table lps (id  bigserial not null, price numeric(19, 2) not null, supplier_id varchar(100) not null, title varchar(100) not null, artist varchar(100) not null, genre varchar(255), primary key (id));
 create table reviews (id  bigserial not null, primary key (id));
 create table users (id  bigserial not null, first_name varchar(100) not null, last_name varchar(100) not null, role varchar(255), primary key (id));
